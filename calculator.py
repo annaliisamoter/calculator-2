eac5b2dec0ee1bc8768b0cf3d8bdaee6094edb9d
Hackbright Student <no-reply@hackbrightacademy.com>
2017-08-23 15:23:07 -0700

    All operations functional

--- calculator.py
+++ calculator.py
@@ -7,24 +7,49 @@ calculator program yourself in this file.
 from arithmetic import *
 
 
-# No setup
-
 while True:
     input = raw_input("> ")
     output = input.split(" ")
 
     command = output[0]
-    num1 = int(output[1])
-    num2 = int(output[2])
+    if len(output) > 1:
+        num1 = int(output[1])
+
+    if len(output) > 2:
+        num2 = int(output[2])
+
+    if len(output) > 3:
+        num3 = int(output[3])
 
     if command == 'q' or command == 'quit':
         break
+
     elif command == '+':
         print add(num1, num2)
-#repeat forever:
-#     read input
-#     tokenize input
-#     if the first token is "q":
-#         quit
-#     else:
-#         decide which math function to call based on first token
+
+    elif command == '-':
+        print subtract(num1, num2)
+
+    elif command == '*':
+        print multiply(num1, num2)
+
+    elif command == '/':
+        print divide(num1, num2)
+
+    elif command == 'square':
+        print square(num1)
+
+    elif command == 'cube':
+        print cube(num1)
+
+    elif command == 'pow':
+        print power(num1, num2)
+
+    elif command == 'mod':
+        print mod(num1, num2)
+
+    elif command == 'x+':
+        print add_mult(num1, num2, num3)
+
+    elif command == 'cubes+':
+        print add_cubes(num1, num2)

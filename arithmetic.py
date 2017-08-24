f82e73b98d5c4d23b60ca64d8e9844d4d4dd62df
Hackbright Student <no-reply@hackbrightacademy.com>
2017-08-23 17:03:29 -0700

    Revised calculation functions to pass in inputs as a list

--- arithmetic.py
+++ arithmetic.py
@@ -5,13 +5,13 @@ def add(numbers):
     """Return the sum of the input integers."""
     total = 0
     for num in numbers:
-        total += int(num)
+        total += num
     return total
 
 
 def subtract(numbers):
-    """Return the second number subtracted from the first."""
-    total = int(numbers[0])
+    """Return the total of integers subracted from the first."""
+    total = numbers[0]
     if len(numbers) > 1:
         new_numbers = numbers[1:]
         for num in new_numbers:
@@ -19,34 +19,46 @@ def subtract(numbers):
     return total
 
 
-def multiply(num1, num2):
-    """Multiply the two inputs together."""
-    return int(num1 * num2)
+def multiply(numbers):
+    """Multiply all inputs together."""
+    total = 1
+    for num in numbers:
+        total = total * num
+    return total
 
 
-def divide(num1, num2):
-    """Divide the first input by the second, returning a floating point."""
-    return float(num1)/num2
+def divide(numbers):
+    """Divide the first input by the all of the following, returning a float."""
+    total = float(numbers[0])
+    if len(numbers) > 1:
+        new_numbers = numbers[1:]
+        for num in new_numbers:
+            total = total / num
+    return total
 
 
-def square(num1):
+def square(numbers):
     """Return the square of the input."""
-    return int(num1 * num1)
+    return numbers[0] * numbers[0]
 
 
-def cube(num1):
+def cube(numbers):
     """Return the cube of the input."""
-    return int(num1 * num1 * num1)
+    return numbers[0] * numbers[0] * numbers[0]
 
 
-def power(num1, num2):
-    """Raise num1 to the power of num2 and return the value."""
-    return int(num1 ** num2)
+def power(numbers):
+    """Raise the first number to the power of the second number and return the value."""
+    if len(numbers) > 2:
+        return "That's too many numbers for this function!"
+    return numbers[0] ** numbers[1]
 
 
-def mod(num1, num2):
-    """Return the remainder of num1 / num2."""
-    return int(num1 % num2)
+def mod(numbers):
+    """Return the remainder of first number divided by second number."""
+    # if len(numbers) > 2:
+    #     return "That's too many numbers for this function!"
+    return numbers[0] % numbers[1]
 
 
 def add_mult(num1, num2, num3):
